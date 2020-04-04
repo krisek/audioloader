eca2b1ca4b417540272bd309b1eb5e558685ed75
Kristof Imre Szabo <kris@lxsystems.de>
2020-04-04 11:23:22 +0200

    bootstrap and angular init

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ import requests
 from requests.packages import urllib3
 import re
 import json
+from flask import jsonify
 from flask import Response
 from flask import render_template
 from app.forms import invoiceForm
@@ -15,6 +16,8 @@ from mpd import MPDClient
 import time
 import random
 from pprint import pprint
+from nested_lookup import nested_lookup
+import datetime
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -43,12 +46,11 @@ def cover():
 
     dir_content = mpd_client.listfiles( request.args.get('directory', ''))
 
-
     app.logger.debug('got dir_content ' + json.dumps(dir_content))
     request_d = request.args.__dict__
 
     image_pattern = re.compile("\.(jpg|jpeg|png|gif)$", re.IGNORECASE)
-    cover_pattern = re.compile("(folder|cover|front)", re.IGNORECASE)
+    cover_pattern = re.compile("front|folder|cover", re.IGNORECASE)
 
     cover = ''
 
@@ -65,17 +67,55 @@ def cover():
             break
         if(cover != ''):
             break
+
     if(cover == '' and images):
         cover = images[0]
-
     if(cover == ''):
         request_d['fullpath'] = '/static/vinyl.png'
         request_d['cover'] = 'vinyl.png'
     else:
-        request_d['fullpath'] = request.args.get('directory', '') + '/' + cover
+        request_d['fullpath'] = app.config['MUSIC_WWW']  + request.args.get('directory', '') + '/' + cover
+        #request_d['fullpath'] = request_d['fullpath'].replace('//','/')
         request_d['cover'] = cover
 
-    return Response(render_template('cover.html', data=request_d))
+    return redirect(request_d['fullpath'])
+    #return Response(renderfrom nested_lookup import nested_lookup_template('cover.html', data=request_d))
+
+@app.route('/listfiles', methods=['GET', 'POST'])
+@app.route('/lsinfo', methods=['GET', 'POST'])
+@app.route('/ls', methods=['GET', 'POST'])
+@app.route('/count', methods=['GET', 'POST'])
+def mpd_proxy():
+    content = {}
+    app.logger.debug('got ' + request.path)
+    if(request.path == '/listfiles'):
+        content['tree'] = mpd_client.listfiles( request.args.get('directory', ''))
+    elif(request.path == '/lsinfo'):
+        content['tree'] = mpd_client.lsinfo( request.args.get('directory', ''))
+    elif(request.path == '/ls'):
+        listfiles = mpd_client.listfiles( request.args.get('directory', ''))
+        lsinfo = mpd_client.lsinfo( request.args.get('directory', ''))
+        count = mpd_client.count('base', request.args.get('directory', ''))
+        music_files = nested_lookup(key='file', document = lsinfo)
+        music_files = list(map(os.path.basename ,music_files))
+        #app.logger.debug('music_files ' + ', '.join(music_files))
+        for file_record in listfiles:
+            if('file' not in file_record):
+                continue
+            #app.logger.debug('file_record ' + file_record['file'])
+            if(file_record.get('file','') not in music_files):
+                file_record['file'] = request.args.get('directory', '') + '/' + file_record['file']
+                lsinfo.append(file_record)
+        content['tree'] = lsinfo
+        content['info'] = count
+    elif(request.path == '/count'):
+        content = mpd_client.count('base', request.args.get('directory', ''))
+        content['playhours'] = re.sub(r'^0:', '', str(datetime.timedelta(seconds=int(content['playtime']))))
+        content['name'] = request.args.get('directory', '')
+
+
+    return jsonify(content)
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
--- config.py
+++ config.py
@@ -5,8 +5,9 @@ class Config(object):
   WTF_CSRF_CHECK_DEFAULT = os.environ.get('WTF_CSRF_CHECK_DEFAULT') or True
   WTF_CSRF_METHODS = []
   MUSIC_DIR = '/media/music/mp3'
-  MUSIC_WWW = 'http://localhost:19000/'
+  MUSIC_WWW = 'http://192.168.1.185:19000/'
   MPD_SERVER = 'localhost'
   MPD_PORT = 6600
   MPD_USER = ''
   MPD_PASSWORD = ''
+  SEND_FILE_MAX_AGE_DEFAULT = 0
