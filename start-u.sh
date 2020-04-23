7bd30605a1156929cebd61b001e2782b9f0334af
Kristof Imre Szabo <kris@lxsystems.de>
2020-04-24 01:00:08 +0200

    standalone start autoscale uwsgi

--- start-u.sh
+++ start-u.sh
@@ -1,1 +1,5 @@
-uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:application --enable-threads --py-autoreload 1
+#!/bin/sh
+
+#start Audioloader Flask application
+
+./venv/bin/uwsgi --socket 0.0.0.0:5000 --protocol=http -w wsgi:application --enable-threads --py-autoreload 1 --processes 5 --plugins-dir /usr/lib/uwsgi/plugins --plugin python3 --workers 64 --cheaper-algo spare --cheaper  8 --cheaper-initial 8 --cheaper-step 4
